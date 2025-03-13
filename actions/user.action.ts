2f8fa7952436ae6ae71d0395de62d35023b981f9
Sumit Raj <sumit.does.code@gmail.com>
2025-03-13 20:13:36 +0530

    removed some imports

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -2,10 +2,6 @@
 
 import User from "@/models/user.model";
 import { connectDB } from "@/db/dbConn";
-import { Edu_AU_VIC_WA_NT_Arrows } from "next/font/google";
-
-// this action is going to create a new user in our database
-// it receives an user objects and saves the user in the database
 
 export const createOrUpdateUser = async (id: any, first_name: any, username: any, email_addresses: any, image_url: any) => {
     try {
