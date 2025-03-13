45a77cce47de09fd2bf8174234093156fdf30e6d
Sumit Raj <sumit.does.code@gmail.com>
2025-03-13 20:02:54 +0530

    fixed bugs

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -2,6 +2,7 @@
 
 import User from "@/models/user.model";
 import { connectDB } from "@/db/dbConn";
+import { Edu_AU_VIC_WA_NT_Arrows } from "next/font/google";
 
 // this action is going to create a new user in our database
 // it receives an user objects and saves the user in the database
@@ -16,7 +17,7 @@ export const createOrUpdateUser = async (id: any, first_name: any, username: any
                     firstName: first_name,
                     username: username,
                     email: email_addresses[0].email,
-                    photo: image_url,
+                    avatar: image_url,
                 },
             },
             { upsert: true, new: true }
